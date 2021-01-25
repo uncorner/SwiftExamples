5c4b11aca3b28070d242213366ba6349589d1bc6
Denis Uncorner <denis.uncorner@gmail.com>
2021-01-25 16:25:05 +0300

    add some tasks to table

--- FirebaseApp/FirebaseApp/TasksViewController.swift
+++ FirebaseApp/FirebaseApp/TasksViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 import Firebase
 
-class TasksViewController: UIViewController {
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var taskTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        taskTableView.delegate = self
+        taskTableView.dataSource = self
+        taskTableView.backgroundColor = UIColor.clear
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        
+        cell.textLabel?.text = "Some task"
+        cell.textLabel?.textColor = UIColor.red
+        cell.backgroundColor = UIColor.clear
+        return cell
+        
     }
 
     @IBAction func signOutTapped(_ sender: UIBarButtonItem) {
