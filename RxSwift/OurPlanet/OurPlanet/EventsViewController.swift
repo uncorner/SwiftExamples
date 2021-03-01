919eacbdda32a8113b4d6159979c0a45a6e98970
uncorner <uncorner.box@gmail.com>
2021-03-01 17:51:10 +0300

    filtered events

--- RxSwift/OurPlanet/OurPlanet/EventsViewController.swift
+++ RxSwift/OurPlanet/OurPlanet/EventsViewController.swift
@@ -42,35 +42,56 @@ class EventsViewController: UIViewController, UITableViewDataSource {
   
   let events = BehaviorRelay<[EOEvent]>(value: [])
   let disposeBag = DisposeBag()
+  
+  let days = BehaviorRelay<Int>(value: 360)
+  let filteredEvents = BehaviorRelay<[EOEvent]>(value: [])
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    events.asObservable()
+    filteredEvents.asObservable()
       .subscribe(onNext: { [weak self] _ in
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { [weak self] in
           self?.tableView.reloadData()
         }
       })
       .disposed(by: disposeBag)
     
+    Observable.combineLatest(days, events) { days, events -> [EOEvent] in
+      let maxInterval = TimeInterval(days * 24 * 3600)
+      return events.filter { event in
+        if let date = event.date {
+          return abs(date.timeIntervalSinceNow) < maxInterval
+        }
+        return true
+      }
+    }
+    .bind(to: filteredEvents)
+    .disposed(by: disposeBag)
+    
+    days.asObservable()
+      .subscribe(onNext: { [weak self] days in
+        self?.daysLabel.text = "Last \(days) days"
+      })
+      .disposed(by: disposeBag)
     
     tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 60
   }
 
   @IBAction func sliderAction(slider: UISlider) {
+    days.accept(Int(slider.value))
   }
 
   // MARK: UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return events.value.count
+    return filteredEvents.value.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
     
-    let event = events.value[indexPath.row]
+    let event = filteredEvents.value[indexPath.row]
     cell.configure(event: event)
     
     return cell
