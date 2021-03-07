c5437a23d7835f4f32e31c3992f25958b8326a17
uncorner <uncorner.box@gmail.com>
2021-03-07 14:39:16 +0300

    add SampleProducts

---
+++ SampleProducts/SampleProducts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProducts
+//
+//  Created by denis on 07.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
