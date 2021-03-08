ed30b0130f6dae1d55d691ab7839f42363808bb9
uncorner <uncorner.box@gmail.com>
2021-03-08 15:21:05 +0300

    refactoring

--- RxSwift/SampleProducts/SampleProducts/ViewController.swift
+++ RxSwift/SampleProducts/SampleProducts/ViewController.swift
@@ -11,38 +11,6 @@ import RxSwift
 class ViewController: UIViewController {
     
     var disposeBag = DisposeBag()
-
-//    func startDownload() {
-//       let eoCategories = EONET.categories
-//
-//       let downloadedEvents = eoCategories
-//         .flatMap { categories in
-//           return Observable.from(categories.map { category in
-//             EONET.events(forLast: 360, category: category)
-//           })
-//         }
-//         .merge(maxConcurrent: 2)
-//
-//       let updatedCategories = eoCategories.flatMap { categories in
-//         downloadedEvents.scan(categories) { updated, events in
-//           return updated.map { category in
-//             let eventsForCategory = EONET.filteredEvents(events: events, forCategory: category)
-//             if !eventsForCategory.isEmpty {
-//               var cat = category
-//               cat.events = cat.events + eventsForCategory
-//               return cat
-//             }
-//             return category
-//           }
-//         }
-//       }
-//
-//       // сначала появляются категории с пустыми ивентами, потом просиходит обновление на категории с заполненными ивентами
-//       eoCategories
-//         .concat(updatedCategories)
-//         .bind(to: categories)
-//         .disposed(by: disposeBag)
-//     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,10 +23,6 @@ class ViewController: UIViewController {
             .share()
         
         let details = products
-//            WebService.products()
-//            .flatMap({ (products) -> Observable<Product> in
-//                return Observable.from(products)
-//            })
             .flatMap { (product) -> Observable<DetailedData> in
                 let detailed = WebService.detailedDataObservable(endpoint: product.src)
                 return detailed
@@ -70,30 +34,30 @@ class ViewController: UIViewController {
                 copyAcc.append(currentItem)
                 return copyAcc
             }
-            .subscribe { (items) in
-                for item in items {
+            .subscribe { (combinedItems) in
+                for item in combinedItems {
                     print("\(item.0.name) | \(item.1.description) | \(item.1.price)")
                 }
-
+                
             } onError: { (error) in
                 print(error.localizedDescription)
             }
             .disposed(by: disposeBag)
-            
-            
-//            .flatMap({ (products:[Product]) -> Observable<[Product]> in
-//                return Observable.just(products)
-//            })
-//            .subscribe { (products) in
-//            for p in products {
-//                print("\(p.name); \(p.src)")
-//            }
-            
-//        } onError: { (error) in
-//            print(error.localizedDescription)
-//        }
     }
-
-
+    
+    //            .flatMap({ (products:[Product]) -> Observable<[Product]> in
+    //                return Observable.just(products)
+    //            })
+    //            .subscribe { (products) in
+    //            for p in products {
+    //                print("\(p.name); \(p.src)")
+    //            }
+    
+    //        } onError: { (error) in
+    //            print(error.localizedDescription)
+    //        }
+    
+    
+    
 }
 
