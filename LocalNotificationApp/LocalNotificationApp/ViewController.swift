bce95704c5c024aa12f02808a30bb941f1c9d228
uncorner <uncorner.box@gmail.com>
2021-07-11 20:42:41 +0300

    add LocalNotificationApp

---
+++ LocalNotificationApp/LocalNotificationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalNotificationApp
+//
+//  Created by denis on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
