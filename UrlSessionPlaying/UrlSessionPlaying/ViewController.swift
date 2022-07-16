57fc5b23b582f13ca59ce820f37f0c085f98962f
uncorner <uncorner.box@gmail.com>
2022-07-16 20:19:21 +0300

    rfc: put logic into WayOne

--- UrlSessionPlaying/UrlSessionPlaying/ViewController.swift
+++ UrlSessionPlaying/UrlSessionPlaying/ViewController.swift
@@ -8,18 +8,20 @@
 import UIKit
 import RxSwift
 
-
-enum NetworkError : Error {
-    case httpResponseCodeError(code: Int)
-}
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var doRequestButton: UIButton!
-    var disposeBag = DisposeBag()
-    
+        
     private var images: [UIImage] = []
+    private lazy var wayOne = getWayOne()
+    
+    private var urls: [URL] = {
+        let url1 = URL(string: "https://kovalut.ru/banklogo/akibank.png?1630431881")!
+        let url2 = URL(string: "https://kovalut.ru/banklogo/avtogradbank.png?1630431881")!
+        let url3 = URL(string: "https://kovalut.ru/banklogo/aziatsko-tihookeanskij-bank.png?1630431881")!
+        return [url1, url2, url3]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,71 +30,19 @@ class ViewController: UIViewController {
         tableView.dataSource = self
     }
 
-    private func getDataTaskObservable(session: URLSession, url: URL) -> DataTaskObservable {
-        let subject = PublishSubject<Data>()
-        
-        let dataTask = session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                subject.onError(error)
-                return
-            }
-            if let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) == false {
-                subject.onError(NetworkError.httpResponseCodeError(code: response.statusCode))
-                return
-            }
-            
-            if let data = data {
-                subject.onNext(data)
-                subject.onCompleted()
-            }
+    @IBAction func onDoRequest(_ sender: Any) {
+        wayOne.run()
+    }
+    
+    private func getWayOne() -> WayOne {
+        let way = WayOne(urls: urls)
+        way.updateImages = { [weak self] images in
+            self?.images.removeAll()
+            self?.images = images
+            self?.tableView.reloadData()
         }
         
-        let dataTaskWrapper = DataTaskObservable(subject: subject, dataTask: dataTask)
-        return dataTaskWrapper
-    }
-
-    @IBAction func onDoRequest(_ sender: Any) {
-        images.removeAll()
-        
-        let session = URLSession(configuration: .default)
-        let url1 = URL(string: "https://kovalut.ru/banklogo/akibank.png?1630431881")!
-        let taskObs1 = getDataTaskObservable(session: session, url: url1)
-        
-        let url2 = URL(string: "https://kovalut.ru/banklogo/avtogradbank.png?1630431881")!
-        let taskObs2 = getDataTaskObservable(session: session, url: url2)
-        
-        let url3 = URL(string: "https://kovalut.ru/banklogo/aziatsko-tihookeanskij-bank.png?1630431881")!
-        let taskObs3 = getDataTaskObservable(session: session, url: url3)
-        
-        Single.zip(taskObs1.single, taskObs2.single, taskObs3.single)
-            .subscribe { [weak self] (data1, data2, data3) in
-                guard let self = self else {
-                    return
-                }
-                print("onSuccess")
-                
-                let datas = [data1, data2, data3]
-                DispatchQueue.main.async {
-                    for data in datas {
-                        if let img = UIImage(data: data) {
-                            self.images.append(img)
-                        }
-                    }
-                    self.tableView.reloadData()
-                    print("reload table view")
-                }
-                
-            } onFailure: { error in
-                print("Error found: \(error.localizedDescription)")
-            } onDisposed: {
-                print("onDisposed")
-            }
-            .disposed(by: disposeBag)
-
-        
-        taskObs1.dataTask.resume()
-        taskObs2.dataTask.resume()
-        taskObs3.dataTask.resume()
+        return way
     }
     
 }
---
+++ UrlSessionPlaying/UrlSessionPlaying/WayOne.swift
@@ -0,0 +1,96 @@
+//
+//  WayOne.swift
+//  UrlSessionPlaying
+//
+//  Created by denis on 16.07.2022.
+//
+
+import Foundation
+import RxSwift
+
+class WayOne {
+    private var disposeBag = DisposeBag()
+    var updateImages: (([UIImage])->Void)?
+    private let urls: [URL]
+    
+    init(urls: [URL]) {
+        self.urls = urls
+    }
+    
+    func run() {
+        let session = URLSession(configuration: .default)
+        let taskObs1 = getDataTaskObservable(session: session, url: urls[0])
+        let taskObs2 = getDataTaskObservable(session: session, url: urls[1])
+        let taskObs3 = getDataTaskObservable(session: session, url: urls[2])
+        
+        Single.zip(taskObs1.single, taskObs2.single, taskObs3.single)
+            .subscribe { [weak self] (data1, data2, data3) in
+                guard let self = self else {
+                    return
+                }
+                print("onSuccess")
+                
+                var images = [UIImage]()
+                let datas = [data1, data2, data3]
+                for data in datas {
+                    if let img = UIImage(data: data) {
+                        images.append(img)
+                    }
+                }
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else {return}
+                    self.updateImages?(images)
+                    print("reload table view")
+                }
+                
+            } onFailure: { error in
+                print("Error found: \(error.localizedDescription)")
+            } onDisposed: {
+                print("onDisposed")
+            }
+            .disposed(by: disposeBag)
+        
+        
+        taskObs1.dataTask.resume()
+        taskObs2.dataTask.resume()
+        taskObs3.dataTask.resume()
+    }
+    
+    private func getDataTaskObservable(session: URLSession, url: URL) -> DataTaskObservable {
+        let subject = PublishSubject<Data>()
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                subject.onError(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) == false {
+                subject.onError(NetworkError.httpResponseCodeError(code: response.statusCode))
+                return
+            }
+            
+            if let data = data {
+                subject.onNext(data)
+                subject.onCompleted()
+            }
+        }
+        
+        let dataTaskWrapper = DataTaskObservable(subject: subject, dataTask: dataTask)
+        return dataTaskWrapper
+    }
+    
+    //MARK: inner classes
+    enum NetworkError : Error {
+        case httpResponseCodeError(code: Int)
+    }
+    
+    struct DataTaskObservable {
+        var subject: PublishSubject<Data>
+        var dataTask: URLSessionDataTask
+        
+        var single: Single<Data> {
+            subject.asSingle()
+        }
+    }
+    
+}
